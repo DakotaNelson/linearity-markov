5c8c8c7f35ec76a42ad30d8624d70ee3dec685c8
Sarah Walters <sarah.walters@students.olin.edu>
2014-04-15 14:32:04 -0400

    adding absorbing case to mark.py

--- mark.py
+++ mark.py
@@ -7,10 +7,35 @@ Created on Tue Apr 15 14:13:17 2014
 
 import numpy
 
-r1 = [1/float(5), 2/float(3), 1/float(4)]
-r2 = [1/float(5), 0/float(3), 1/float(4)]
-r3 = [3/float(5), 1/float(3), 2/float(4)]
+def makeRegular():
+    ''' generates regular transition matrix to represent Mark's clothing choices
+        input: none
+        output: numpy array
+    '''
+    r1 = [1/float(5), 2/float(3), 1/float(4)]
+    r2 = [1/float(5), 0/float(3), 1/float(4)]
+    r3 = [3/float(5), 1/float(3), 2/float(4)]
 
-arrayL = [r1, r2, r3]
-L = numpy.matrix(arrayL)
-print L**100
+    arrayL = [r1, r2, r3]
+    return numpy.matrix(arrayL)
+    
+def makeAbsorbing():
+    ''' generates absorbing transition matrix to represent Mark's clothing choices
+        input: none
+        output: numpy array
+    '''
+    r1 = [1, 2/float(3), 1/float(4)]
+    r2 = [0, 0/float(3), 1/float(4)]
+    r3 = [0, 1/float(3), 2/float(4)]
+    
+    arrayL = [r1, r2, r3]
+    return numpy.matrix(arrayL)
+    
+def predict(L, k, currentState): # current state in [[1],[0],[0]] format - column matrix
+    ''' predicts probabilities k states from currentState
+        input: numpy transition matrix array L, integer k, current state vector
+            currentState must be in format [[1],[0],[0]]
+        output: numpy array representing probabilites k states from currentState
+    '''
+    x = numpy.matrix(currentState)
+    return L**k*x
