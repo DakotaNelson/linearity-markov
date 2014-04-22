99420537bbd5eb376e77cde295ac571d74f7728c
Pratool <pratool.gadtaula@students.olin.edu>
2014-04-22 13:39:56 -0400

    Testing with sound

--- music.py
+++ music.py
@@ -43,3 +43,12 @@ def samplingWheel(a):
         return numpy.matrix([[0],[1],[0]])
     else:
         return numpy.matrix([[0],[0],[1]])
+
+import numpy as np
+from scipy.io.wavfile import write
+
+data = np.random.uniform(-1,1,44100) # 44100 random samples between -1 and 1
+print data
+scaled = np.int16(data/np.max(np.abs(data)) * 32767)
+print scaled
+write('test.wav', 44100, scaled)
